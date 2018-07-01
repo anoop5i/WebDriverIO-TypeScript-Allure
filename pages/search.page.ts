3d1657af27f2af0388ba5a1bab6ad7df6af1b03c
anoopsivarajan@outlook.com <anoopsivarajan@outlook.com>
2018-07-01 22:10:14 +0100

    Removed invalid elements

--- pages/search.page.ts
+++ pages/search.page.ts
@@ -1,16 +1,4 @@
 class Search_Page {
-  public get username() {
-    return browser.element("#username");
-  }
-  public get password() {
-    return browser.element("#password");
-  }
-  public get form() {
-    return browser.element("#login");
-  }
-  public get flash() {
-    return browser.element("#flash");
-  }
   public get search() {
     return browser.element('//input[@name="q"]');
   }
@@ -21,12 +9,6 @@ class Search_Page {
 
   public open(): void {
     browser.url("/login");
-  }
-  /**
-   *
-   */
-  public submit(): void {
-    this.form.submitForm();
   }
 
   public setSearch(item: string): void {
